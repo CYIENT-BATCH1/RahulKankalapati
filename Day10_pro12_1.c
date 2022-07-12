d1f4454e2a5f6fb7723f9645ff7e3a6d4466e100
Rahul Kankalapati <108972262+rk69063@users.noreply.github.com>
2022-07-12 07:30:46 +0530

    Add files via upload

---
+++ Day10_pro12_1.c
@@ -0,0 +1,40 @@
+12.Write a program to Implement ”Selection Sort” using Recursion for given elements (Number elements will vary, so make it as configurable item)
+How Selection sort works is explained below.
+Program:
+#include<stdio.h>
+void sortarrayelement(int arr[], int num)
+{
+    int i,j;
+    if(num>0)
+    {
+    int i,j,temp;
+     for(j=0;arr[j];j++)
+    {
+    for(i=j+1;arr[i];i++)
+    {
+        if(arr[i]<arr[j])
+        {
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    }
+}
+}
+int main()
+{
+    int num=5,i;
+    int arr[num];
+    printf("Enter the Array Element:");
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    sortarrayelement(arr,num);
+    printf("After sort Array Elements are:");
+    int j;
+    for (j=0; j< num; j++)
+     printf("%d ", arr[j]);
+    return 0;
+}
